efd81be016e169cb721dd323ecd0efd2284c3103
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-20 14:48:27 +0100

    create  generate invoice button

---
+++ src/components/sidebar/invoice/invoice.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './invoice.css';
+
+function Invoice() {
+  return (
+    <div className="invoice">
+      GENERATE INVOICE
+    </div>
+  );
+}
+
+export default Invoice;
