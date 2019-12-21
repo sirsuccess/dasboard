08696ebaf5116c9e62489979b85ad5fe0ec26385
sirsuccess <Kanuamani@gmail.com>
2019-12-21 10:01:16 +0100

    comment out error code

--- src/commons/progressBar.js
+++ src/commons/progressBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar'
+// import ProgressBar from 'react-bootstrap/ProgressBar'
 
 const ProgressLine = (props) => {
   return (
     <div>
-     <div class="w3-yellow w3-round-xlarge">
-    <div class="w3-container w3-blue w3-round-xlarge" style={{width:"25%"}}>25%</div>
-  </div>
+//      <div class="w3-yellow w3-round-xlarge">
+//     <div class="w3-container w3-blue w3-round-xlarge" style={{width:"25%"}}>25%</div>
+//   </div>
     </div>
   );
 };
