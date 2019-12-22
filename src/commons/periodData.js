0d1efc32992c31fc46293a2ef4c7f5a81bcc68a2
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-22 12:06:20 +0100

    rearrange data

--- src/commons/periodData.js
+++ src/commons/periodData.js
@@ -1,21 +1,20 @@
 export default {
-    Data: [
-      {
-        title: 'Daily Transaction Volume',
-        value: "2,342",
-      },
-      {
-        title: 'Daily Transaction Value',
-        value: "4,000,000",
-      },
-      {
-        title: 'Total Transaction Volume',
-        value: "452,000",
-      },
-      {
-        title: 'Total Transaction Value',
-        value: "4,000,000",
-      },
-      
-    ],
-  };
+  Data: [
+    {
+      title: "Daily Transaction Volume",
+      value: "2,342"
+    },
+    {
+      title: "Daily Transaction Value",
+      value: "4,000,000"
+    },
+    {
+      title: "Total Transaction Volume",
+      value: "452,000"
+    },
+    {
+      title: "Total Transaction Value",
+      value: "4,000,000"
+    }
+  ]
+};
