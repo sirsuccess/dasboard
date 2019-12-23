6d6257429623c99167550e3faa2adb07fa714bbc
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-23 18:23:32 +0100

    move date to a separate component

--- src/commons/date.js
+++ src/commons/date.js
@@ -3,22 +3,24 @@
 
 function CurrentDate() {
     
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1; //January is 0!
-        console.log(mm);
-        
-        const yyyy = today.getFullYear();
-        if (dd < 10) {
-          dd = "0" + dd;
-        }
-        if (mm < 10) {
-          mm = "0" + mm;
-        }
-        today = dd + "/" + mm + "/" + yyyy;
-  return (
-    {today}
-  );
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  console.log(mm);
+  
+  const yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  const monthArr = ["Jan","Feb", "Mar","Apr","May","Jun", "Jul","Aug", "Sept", "Oct","Nov","Dec"]
+  
+  mm=monthArr[mm-1]
+  today = dd + ", " + mm + " " + yyyy;
+  return today;
 }
 
 export default CurrentDate;
