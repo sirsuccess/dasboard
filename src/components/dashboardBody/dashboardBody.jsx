21d4d213d1388a158a1d07def082affb637ec864
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-26 21:17:42 +0100

    change import direction link

--- src/components/dashboardBody/dashboardBody.jsx
+++ src/components/dashboardBody/dashboardBody.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import PeriodLayout from "./periodLayout/periodLayout"
 import ChartSection from "./chartSection/chartSection"
-import Table from "./table"
+import Table from "./table/table"
+import TableCustome from "./tableCostume"
 import './dashboardBody.css';
 
 function DashboardBody() {
@@ -10,8 +11,9 @@ function DashboardBody() {
     <>
     <PeriodLayout/>
     <ChartSection/>
-    <h3 style={{fontSize:"36px", marginLeft:"0.5em", marginBottom:"1em"}}>Payments</h3>
-    <div className="tableClass"><Table/></div> 
+    <p className="paymentClass">Payments</p>
+    {/* <div className="tableClass"><Table/></div>  */}
+    <TableCustome/>
     </>
   );
 }
