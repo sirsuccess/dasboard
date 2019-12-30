9c816b294cec3212fb6821bedc3012d41692e775
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-30 10:01:51 +0100

    to resolve netlify build error

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard"
+import Dashboard from "../pages/dashboard/Dashboard"
 
 function Routers() {
   return (
